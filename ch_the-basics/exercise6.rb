be050e80d20ab2701ee6ff9b785a36289df58ed5
aaroncallagher <me@aaroncallagher.com>
2016-07-22 18:41:44 +0000

    Added Exercise 6 Solution

---
+++ ch_the-basics/exercise6.rb
@@ -0,0 +1,12 @@
+print "Enter a decimal number:"
+user_number = gets.to_f
+
+print "Enter another decimal number:"
+user_number2 = gets.to_f
+
+print "Enter another decimal number:"
+user_number3 = gets.to_f
+
+puts user_number * user_number
+puts user_number2 * user_number2
+puts user_number3 * user_number3
