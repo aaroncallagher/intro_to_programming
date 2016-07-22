29375e0a61d31ae1323729dcf40ff81e6c01e300
aaroncallagher <me@aaroncallagher.com>
2016-07-22 16:17:37 +0000

    Exercise 3 Solution added

---
+++ ch_the-basics/exercise3.rb
@@ -0,0 +1,7 @@
+movie_list = {:Fargo => 1996, :The_Usual_Suspects => 1995, :Twelve_Angry_Men => 1957, :City_of_God => 2002, :The_Departed => 2006}
+
+puts movie_list[:Fargo]
+puts movie_list[:The_Usual_Suspects]
+puts movie_list[:Twelve_Angry_Men]
+puts movie_list[:City_of_God]
+puts movie_list[:The_Departed]
