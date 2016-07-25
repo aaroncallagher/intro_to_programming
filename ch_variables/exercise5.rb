545b9d1b6514af2fc4dd615c6c05b366aede2b18
aaroncallagher <me@aaroncallagher.com>
2016-07-25 22:09:27 +0000

    Added Exercise 5 & 6 of Variables Chapter

---
+++ ch_variables/exercise5.rb
@@ -0,0 +1,2 @@
+# First Program Will Output 3 to the Screen
+#Second Program Will Output An Error, Because It Is Not Available in the Global Scope
---
+++ ch_variables/exercise6.rb
@@ -0,0 +1,1 @@
+#Program is trying to refernce a variable that is inaccessible or doesnt exist in the current scope
