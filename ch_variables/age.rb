6d426d2252138363f7be24da63b8851bf56d7b92
aaroncallagher <me@aaroncallagher.com>
2016-07-25 22:04:15 +0000

    Added Variables Chapter Exercise Solutions

---
+++ ch_variables/age.rb
@@ -0,0 +1,7 @@
+print "How old are you? "
+age = gets.to_i
+
+puts "In 10 years you will be " + (age + 10).to_s
+puts "In 20 years you will be " + (age + 20).to_s
+puts "In 30 years you will be " + (age + 30).to_s
+puts "In 40 years you will be " + (age + 40).to_s
---
+++ ch_variables/name.rb
@@ -0,0 +1,13 @@
+print "Enter Your First Name: "
+first_name = gets.chomp
+
+print "Enter your Last Name: "
+last_name = gets.chomp
+
+puts "How do You Do, #{first_name} #{last_name}?"
+
+full_name = first_name + " " + last_name
+
+10.times do
+  puts full_name
+end
