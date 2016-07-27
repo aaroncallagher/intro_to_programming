d369efe906da4f76ad2abe3f85b4a7c8c3c12b7c
aaroncallagher <me@aaroncallagher.com>
2016-07-27 18:20:46 +0000

    Added Flow Control Exercise Solution 1

---
+++ ch_flow-control/exercise1.rb
@@ -0,0 +1,6 @@
+# 1.) Will evaluate to false, because 32*4 = 128, which is not greater than or equal to 129
+# 2.) Will evalute to false, because false IS equal to false (which is tthe equivalent of !true)
+# 3.) Will evaluate to false, because one true is a boolean and 4 is an integer
+# 4.) Will evaluate to true, because false == false (847 == '847') evaluates to false.
+# 5.) will evaluate to true, pay CLOSE attention to parentheses. When you evaluate it down it becomes
+#    (false || true) || false >>>> true || false >>>> true
