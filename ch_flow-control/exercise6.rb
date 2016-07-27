b30e4fc8c85db68c47978d85515728e124e0f178
aaroncallagher <me@aaroncallagher.com>
2016-07-27 21:19:18 +0000

    Added Exercise 6 Solution to Flow Control Chapter Exercises

---
+++ ch_flow-control/exercise6.rb
@@ -0,0 +1,12 @@
+#You get the error because the if/else statement needs to have an end as well as the method definition.
+#In order to fix, you can add an end at same indent as the if / else, directly after "puts nope"
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
