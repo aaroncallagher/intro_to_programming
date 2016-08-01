18122ba833758802cf400d0557b8506e50bd5e07
aaroncallagher <me@aaroncallagher.com>
2016-08-01 22:22:20 +0000

    Added Solutions to Exercise 3 & 4

---
+++ ch_arrays/exercise3.rb
@@ -0,0 +1,5 @@
+arr = [["test", "hello", "world"], ["example", "mem"]]
+
+arr = arr.flatten
+
+puts arr[3]
---
+++ ch_arrays/exercise4.rb
@@ -0,0 +1,5 @@
+# 1.) Will return 3, because index returns the index of the selected value
+
+# 2.) Returns an undefined method '[]' error
+
+# 3.) Returns 8, the value of the 5 index of the array arr
