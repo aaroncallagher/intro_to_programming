81c0a1e3ae035761c5c1b4eda637b283b24c2846
aaroncallagher <me@aaroncallagher.com>
2016-08-02 21:26:52 +0000

    Added Exercise 7 Solution

---
+++ ch_hashes/exercise7.rb
@@ -0,0 +1,9 @@
+# x = "hi"
+#  => "hi" 
+# my_hash = {x: "value"}
+#  => {:x=>"value"} 
+# my_hash2 = {x => "value"}
+#  => {"hi"=>"value"} 
+
+# As we see from the return values, my_hash2's key references the value of the variable x, 
+#  ("hi there") whereas my_hash creates a symbol x (:x) as a key
