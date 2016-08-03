47b8c54191b2f1d022a14f5146ede804f499042d
aaroncallagher <me@aaroncallagher.com>
2016-08-03 23:56:51 +0000

    Added Exercise 10 & 11 Solutions

---
+++ ch_exercises/exercise10.rb
@@ -0,0 +1,5 @@
+# Absolutely they can!
+
+family = { uncles: ["tom", "dick", "harry"], aunts: ["beverly", "mildred"] }
+
+my_stats = [ {height: "6'7"}, {weight: "215 lbs"}, {eyes: "blue"} ]
---
+++ ch_exercises/exercise11.rb
@@ -0,0 +1,2 @@
+# I liked Fullscreen's YouTube API Ruby Client (https://github.com/Fullscreen/yt)
+# because the documentation was thorough and relatively easy to understand
